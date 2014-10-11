a43a1cb40fee9dde9f42a707096bbd4bd101c3e7
Dmytro Morozov <dmm@timemsystem.com>
2014-10-11 17:45:09 +0300

    Removed extras in database and classes.

--- WebApplication/Models/DbEventEntry.cs
+++ WebApplication/Models/DbEventEntry.cs
@@ -6,7 +6,6 @@ namespace WebApplication.Models
     public class DbEventEntry
     {
         public int EventId { get; set; }
-        public string EventName { get; set; }
         public DateTimeOffset Timestamp { get; set; }
         public Guid ActivityId { get; set; }
         public Guid ProviderId { get; set; }
@@ -19,30 +18,23 @@ namespace WebApplication.Models
         public int Version { get; set; }
         public long Keywords { get; set; }
         public string FormattedMessage { get; set; }
-        public string KeywordsDescription { get; set; }
-        public string OpcodeName { get; set; }
         public string Payload { get; set; }
         public string ProviderName { get; set; }
-        public string TaskName { get; set; }
 
         public DbEventEntry(EventEntry entry, string payload)
         {
             ActivityId = entry.ActivityId;
             EventId = entry.EventId;
-            EventName = entry.Schema.EventName;
             FormattedMessage = entry.FormattedMessage;
             Keywords = (long)entry.Schema.Keywords;
-            KeywordsDescription = entry.Schema.KeywordsDescription;
             Level = (int)entry.Schema.Level;
             Opcode = (int)entry.Schema.Opcode;
-            OpcodeName = entry.Schema.OpcodeName;
             Payload = payload;
             ProcessId = entry.ProcessId;
             ProviderId = entry.ProviderId;
             ProviderName = entry.Schema.ProviderName;
             RelatedActivityId = entry.RelatedActivityId;
             Task = (int)entry.Schema.Task;
-            TaskName = entry.Schema.TaskName;
             ThreadId = entry.ThreadId;
             Timestamp = entry.Timestamp;
             Version = entry.Schema.Version;
